4a3b90599311290d16710cb661ed886c70418980
phamducit <phamducit999@gmail.com>
2018-10-27 01:03:14 +0700

    Phước Kiên - Login

--- app/src/main/java/com/example/mrrs/schoolhelper/LoginActivity.java
+++ app/src/main/java/com/example/mrrs/schoolhelper/LoginActivity.java
@@ -9,14 +9,28 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.mrrs.schoolhelper.model.Login;
+import com.example.mrrs.schoolhelper.service.APIService;
+import com.example.mrrs.schoolhelper.service.Dataservice;
 import com.example.mrrs.schoolhelper.welcome.PrefManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button forgotPassword, btn_login;
     PrefManager prefManager;
-
+    EditText edt_user, edt_pass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,16 +39,18 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         setContentView(R.layout.activity_login);
         initComponent();
         setOnClickListener();
-        prefManager = new PrefManager(LoginActivity.this);
-        if (!prefManager.isFirstTimeLogin()) {
-            changeScreen(LoginActivity.this, HomeActivity.class);
-            finish();
-        }
+//        prefManager = new PrefManager(LoginActivity.this);
+//        if (!prefManager.isFirstTimeLogin()) {
+//            changeScreen(LoginActivity.this, HomeActivity.class);
+//            finish();
+//        }
     }
 
     public void initComponent() {
         forgotPassword = findViewById(R.id.login_btn_forgot);
         btn_login = findViewById(R.id.login_btn_login);
+        edt_user = findViewById(R.id.login_edt_username);
+        edt_pass = findViewById(R.id.login_edt_pass);
     }
 
     public void setOnClickListener() {
@@ -49,10 +65,31 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 changeScreen(LoginActivity.this, ForgotPasswordActivity.class);
                 break;
             case R.id.login_btn_login:
-                changeScreen(LoginActivity.this, HomeActivity.class);
                 // set for auto next screen if login yet
-                prefManager.setFirstTimeLogin(false);
-                break;
+                String user = edt_user.getText().toString();
+                String pass = edt_pass.getText().toString();
+                if(user.length() > 0 && pass.length() >0){
+                    Dataservice dataservice = APIService.getService();
+                    Call<List<Login>> callback = dataservice.Login(user,pass);
+                    callback.enqueue(new Callback<List<Login>>() {
+                        @Override
+                        public void onResponse(Call<List<Login>> call, Response<List<Login>> response) {
+                            ArrayList<Login> loginArrayList = (ArrayList<Login>) response.body();
+                            if(loginArrayList.size() > 0){
+                                changeScreen(LoginActivity.this, HomeActivity.class);
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<List<Login>> call, Throwable t) {
+                            Toast.makeText(LoginActivity.this, "The account or password is incorrect", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }else{
+                    Toast.makeText(this, "Not fully entered", Toast.LENGTH_SHORT).show();
+                }
+//                prefManager.setFirstTimeLogin(false);
+//                break;
         }
     }
 
--- app/src/main/java/com/example/mrrs/schoolhelper/service/Dataservice.java
+++ app/src/main/java/com/example/mrrs/schoolhelper/service/Dataservice.java
@@ -1,12 +1,21 @@
 package com.example.mrrs.schoolhelper.service;
 import com.example.mrrs.schoolhelper.model.LocationMap;
+import com.example.mrrs.schoolhelper.model.Login;
 import com.example.mrrs.schoolhelper.model.Student;
 import java.util.List;
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
+
 public interface Dataservice {
     @GET("student.php")
     Call<List<Student>> GetDataInfoStudent();
     @GET("location.php")
     Call<List<LocationMap>> GetDataLocation();
+
+    @FormUrlEncoded
+    @POST("login.php")
+    Call<List<Login>> Login(@Field("user") String user, @Field("pass") String pass);
 }
