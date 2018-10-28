88c24bfa1e31096191d4fccaac1b0e6df755bc34
nguyenxuankien1997 <kiennxps06257@fpt.edu.vn>
2018-10-28 14:08:14 +0700

    Forgot Password

--- app/src/main/java/com/example/mrrs/schoolhelper/ForgotPasswordActivity.java
+++ app/src/main/java/com/example/mrrs/schoolhelper/ForgotPasswordActivity.java
@@ -6,8 +6,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.mrrs.schoolhelper.service.APIConnect;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
+
+
+    EditText edt_forgotemail;
+    Button btnsubmitReset;
+    String forgotemail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,5 +46,60 @@ public class ForgotPasswordActivity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), LoginActivity.class));
             }
         });
+
+        Anhxa();
+        btnsubmitReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                forgotemail =edt_forgotemail.getText().toString();
+                if(forgotemail.isEmpty()){
+                    Toast.makeText(ForgotPasswordActivity.this, "Not fully entered", Toast.LENGTH_SHORT).show();
+                }else {
+                    SubmitForgotPassword(APIConnect.URL_FORGOT);
+                }
+            }
+
+
+        });
+    }
+
+    private void SubmitForgotPassword(String urlForgot) {
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, urlForgot,
+
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        if(response.equals("OK")){
+                            Toast.makeText(ForgotPasswordActivity.this, "Success", Toast.LENGTH_SHORT).show();
+                            edt_forgotemail.setText("");
+
+                        }else {
+                            Toast.makeText(ForgotPasswordActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }
+        ){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+
+                params.put("email",forgotemail);
+
+                return params;
+            }
+        };
+        requestQueue.add(stringRequest);
+    }
+
+    private void Anhxa() {
+        edt_forgotemail = (EditText) findViewById(R.id.forgot_edt_email);
+        btnsubmitReset =(Button) findViewById(R.id.forgot_btn_submit);
     }
 }
